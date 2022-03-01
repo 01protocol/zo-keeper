bbe60eac7fd39cd6c5287a4a8546960f0f0c5b9c
Bit Tensor <>
2022-03-01 16:46:38 -0500

    Remove dev files

--- build-both.sh
+++
@@ -1,1 +0,0 @@
-CARGO_TARGET_DIR=target/mainnet cargo build --release & CARGO_TARGET_DIR=target/devnet cargo build --features devnet --release
--- run-dev.sh
+++
@@ -1,4 +0,0 @@
-./target/devnet/release/zo-keeper \
-    --rpc-url "https://psytrbhymqlkfrhudd.dev.genesysgo.net:8899/" \
-    --ws-url "wss://psytrbhymqlkfrhudd.dev.genesysgo.net:8900/" \
-    liquidator --worker-count 1 --worker-index 0 | tee $1
